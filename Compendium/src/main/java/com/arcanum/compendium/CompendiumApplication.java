f9c39a568f7f606b8605b47629c2622864bc3372
nelisriebezos <niels@riezebos.net>
2024-06-15 23:44:50 +0200

    init

---
+++ Compendium/src/main/java/com/arcanum/compendium/CompendiumApplication.java
@@ -0,0 +1,13 @@
+package com.arcanum.compendium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompendiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompendiumApplication.class, args);
+    }
+
+}
