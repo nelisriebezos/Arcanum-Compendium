7cefc3d226a142d3d2102ab48f2a7a4b56791d16
nelisriebezos <niels@riezebos.net>
2024-07-12 02:39:56 +0200

    kunnen beginnen met test data toevoegen

--- src/main/java/com/nelis/compendium/javafx/controllers/StatsPaneController.java
+++ src/main/java/com/nelis/compendium/javafx/controllers/StatsPaneController.java
@@ -1,12 +1,13 @@
 package com.nelis.compendium.javafx.controllers;
 
+import com.nelis.compendium.Main;
 import com.nelis.compendium.core.service.PlayerCharacterService;
 import com.nelis.compendium.javafx.dtos.MainSkillDTO;
 import com.nelis.compendium.javafx.dtos.SubSkillDTO;
 import javafx.fxml.FXML;
 import javafx.geometry.Orientation;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
+import javafx.geometry.Insets;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -17,27 +18,25 @@ import java.util.List;
 @Component
 public class StatsPaneController {
     private final PlayerCharacterService playerCharacterService;
+    private final Main main;
 
     @FXML
     private VBox skillsList;
 
-    public StatsPaneController(PlayerCharacterService playerCharacterService) {
+    public StatsPaneController(PlayerCharacterService playerCharacterService, Main main) {
         this.playerCharacterService = playerCharacterService;
+        this.main = main;
     }
 
     @FXML
     private void initialize() {
         SubSkillDTO skill = new SubSkillDTO("Acrobatics", 2, false);
         SubSkillDTO skill2 = new SubSkillDTO("Animal Handling", -2, true);
-        SubSkillDTO skill3 = new SubSkillDTO("Arcana", 0, false);
-        SubSkillDTO skill4 = new SubSkillDTO("Athletics", 1, true);
+        SubSkillDTO skill3 = new SubSkillDTO("Arcana", +5, false);
+        SubSkillDTO skill4 = new SubSkillDTO("Athletics", 1, false);
         MainSkillDTO ms1 = new MainSkillDTO("Strength", 10, 0, List.of(skill, skill2));
         MainSkillDTO ms2 = new MainSkillDTO("Dexterity", 10, -2, List.of(skill3, skill4));
-        MainSkillDTO ms3 = new MainSkillDTO("Constitution", 10, 0, List.of(skill, skill2));
-        MainSkillDTO ms4 = new MainSkillDTO("Intelligence", 10, 5, List.of(skill3, skill4));
-        MainSkillDTO ms5 = new MainSkillDTO("Wisdom", 10, 0, List.of(skill, skill2));
-        MainSkillDTO ms6 = new MainSkillDTO("Charisma", 10, 0, List.of(skill3, skill4));
-        updateSkillBox(List.of(ms1, ms2, ms3, ms4, ms5, ms6));
+        updateSkillBox(List.of(ms1, ms2));
     }
 
     private String getSkillColor(String name) {
@@ -58,28 +57,52 @@ public class StatsPaneController {
 
         AnchorPane mainSkillPane = new AnchorPane();
         HBox mainSkillRow = new HBox();
+        mainSkillRow.setSpacing(10);
+        mainSkillRow.setPadding(new Insets(5));
+        mainSkillRow.setStyle("-fx-background-color: " + getSkillColor(dto.name()) + ";");
 
-        String plusMinusForSkill = " ";
-        if (dto.baseModifier() > 0) plusMinusForSkill = " +";
+        String plusMinusForSkill = "  ";
+        if (dto.baseModifier() > 0) plusMinusForSkill = "+";
 
         Label mainSkillName = new Label(dto.name());
-        Label mainSkillValue = new Label(plusMinusForSkill + dto.baseValue());
+        mainSkillName.setPrefWidth(100);
+        mainSkillName.setStyle("-fx-font-weight: bold;");
+
+        Label mainSkillValue = new Label("" + dto.baseValue());
+        mainSkillValue.setPrefWidth(35);
+        mainSkillValue.setStyle("-fx-font-weight: bold;");
+
         Label mainSkillModifier = new Label(plusMinusForSkill + dto.baseModifier());
+        mainSkillModifier.setPrefWidth(35);
+        mainSkillModifier.setStyle("-fx-font-weight: bold;");
 
         mainSkillRow.getChildren().addAll(mainSkillName, mainSkillValue, mainSkillModifier);
         mainSkillPane.getChildren().add(mainSkillRow);
 
         AnchorPane subSkillPane = new AnchorPane();
         VBox subSkillList = new VBox();
+        subSkillList.setSpacing(10);
+        subSkillList.setPadding(new Insets(5));
 
         for (SubSkillDTO subSkill : dto.subSkills()) {
+
             String plusMinusForSubSkill = " ";
-            if (dto.baseModifier() > 0) plusMinusForSubSkill = " +";
+            if (subSkill.modifier() > 0) plusMinusForSubSkill = "+";
+
 
             HBox skillRow = new HBox();
+            skillRow.setSpacing(10);
+            skillRow.setPadding(new Insets(5));
+
             Label subSkillName = new Label(subSkill.name());
+            subSkillName.setPrefWidth(100);
+
             Label subSkillValue = new Label(plusMinusForSubSkill + subSkill.modifier());
+            subSkillValue.setPrefWidth(35);
+
             CheckBox proficiencyButton = new CheckBox();
+            subSkillValue.setPrefWidth(35);
+
             if (subSkill.isProficient()) proficiencyButton.setSelected(true);
             proficiencyButton.setOnAction(event -> System.out.println("you clicked button"));
 
@@ -91,6 +114,9 @@ public class StatsPaneController {
 
         tablePane.getItems().add(mainSkillPane);
         tablePane.getItems().add(subSkillPane);
+
+        SplitPane.setResizableWithParent(mainSkillPane, false);
+        SplitPane.setResizableWithParent(subSkillPane, false);
         return tablePane;
     }
 
@@ -99,7 +125,7 @@ public class StatsPaneController {
             SplitPane table = createSkillTable(dto);
             skillsList.getChildren().add(table);
         }
-
+//
 //        for (MainSkillDTO mainSkill : mainSkillData) {
 //            String plusMinus = " ";
 //            if (mainSkill.baseModifier() > 0) plusMinus = " +";
@@ -112,7 +138,6 @@ public class StatsPaneController {
 //                    "-fx-padding: 5;");
 //
 //            Label mainSkillName = new Label(mainSkill.name());
-//            mainSkillName.setStyle("-fx-font-weight: bold; -fx-padding: 0 10 0 0;");
 //
 //            Label mainSkillValue = new Label("" + mainSkill.baseValue());
 //            mainSkillValue.setStyle("-fx-font-weight: bold; -fx-padding: 0 10 0 0;");
@@ -123,23 +148,6 @@ public class StatsPaneController {
 //            mainSkillRow.getChildren().addAll(mainSkillName, mainSkillValue, mainSkillModifier);
 //            skillsList.getChildren().add(mainSkillRow);
 //
-//            // Add skills to the skills list
-//            for (SubSkillDTO subSkill : mainSkill.subSkills()) {
-//                String plusMinusForSkill = " ";
-//                if (mainSkill.baseModifier() > 0) plusMinusForSkill = " +";
-//
-//                HBox skillRow = new HBox();
-//                Label subSkillName = new Label(subSkill.name());
-//
-//                Label subSkillValue = new Label(plusMinusForSkill + subSkill.modifier());
-//
-//                CheckBox proficiencyButton = new CheckBox();
-//                if (subSkill.isProficient()) proficiencyButton.setSelected(true);
-//                proficiencyButton.setOnAction(event -> System.out.println("you clicked button"));
-//
-//                skillRow.getChildren().addAll(subSkillName, subSkillValue, proficiencyButton);
-//                skillsList.getChildren().add(skillRow);
-//            }
-//        }
+//    }
     }
 }
